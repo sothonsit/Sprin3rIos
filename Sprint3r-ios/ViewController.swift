4bcc477cc576ff9d169cc6dda418ed80ec51e6a4
Sothon <Sothon@odd.works>
2017-07-29 11:54:55 +0700

    create ios

--- Sprint3r-ios/ViewController.swift
+++ Sprint3r-ios/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var leftOperandTextField: UITextField!
+    @IBOutlet weak var rightOperandTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func calulateButtonClicked(_ sender: UIButton) {
+        let leftOperand = self.leftOperandTextField.text ?? "0"
+        let rightOperand = self.rightOperandTextField.text ?? "0"
+        let left = Int(leftOperand) ?? 0
+        let right = Int(rightOperand) ?? 0
+        
+        self.resultLabel.text = String(left + right)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
