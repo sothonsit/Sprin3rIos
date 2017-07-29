3646345139d23ba20c810d7f37cf468209912b23
Sothon <Sothon@odd.works>
2017-07-29 10:04:48 +0700

    Initial Commit

---
+++ Sprint3r-iosTests/Sprint3r_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  Sprint3r_iosTests.swift
+//  Sprint3r-iosTests
+//
+//  Created by Jester No'Future on 7/29/17.
+//  Copyright © 2017 Jester No'Future. All rights reserved.
+//
+
+import XCTest
+@testable import Sprint3r_ios
+
+class Sprint3r_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
